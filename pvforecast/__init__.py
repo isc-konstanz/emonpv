3b4e805902f9440357a1cb64ffd3339f01df18d9
Adrian Minde <Adrian_Minde@live.de>
2017-12-12 16:55:14 +0100

    Update readme and descriptions

--- pvforecast/__init__.py
+++ pvforecast/__init__.py
@@ -5,7 +5,7 @@
     
     PV forecast provides a set of functions to forecast the energy generation of photovoltaic system.
     To improve the prediction performance, the recursive optimization of hourly efficiency values may be used.
-    It utilizes the independent PVLIB toolbox, originally developed in MATLAB at Sandia National Laboratories,
+    It utilizes the independent pvlib-python toolbox, originally developed in MATLAB at Sandia National Laboratories,
     and can be found on GitHub "https://github.com/pvlib/pvlib-python".
     
 """
