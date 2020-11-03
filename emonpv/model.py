2dbed33127971b3ac4a65a2e7b82e0e6e3d1a0e2
Adrian Minde <Adrian_Minde@live.de>
2020-11-03 17:01:20 +0100

    Update dependencies to th-e-core v0.1.5

--- emonpv/model.py
+++ emonpv/model.py
@@ -8,68 +8,37 @@
 import logging
 logger = logging.getLogger(__name__)
 
-#import os
-import numpy as np
+import os
 import pandas as pd
 
-from pvlib import modelchain
-from th_e_core import Model
+from pvlib.modelchain import ModelChain
+from th_e_core import Model as ModelCore
 
 
-class OpticalModel(Model):
+class Model(ModelChain, ModelCore):
 
-    def _configure(self, configs, **kwargs):
-        super()._configure(configs, **kwargs)
-        self.threading = configs.get('General', 'threading', fallback='true').lower() == 'true'
-        self.parameters = self._configs.items('Model')
+    @classmethod
+    def read(cls, context, system, **kwargs):
+        configs = cls.read_configs(system, **kwargs)
+        configs_override = os.path.join(configs['General']['config_dir'], 
+                                        system.id+'.d', 'model.cfg')
+        
+        if os.path.isfile(configs_override):
+            configs.read(configs_override)
+        
+        type = configs.get('General', 'type', fallback='default') #@ReservedAssignment
+        if type.lower() in ['default', 'optical', 'pvlib']:
+            return Model(system, context.location, configs, **kwargs)
+        
+        return cls.from_configs(system, configs, **kwargs)
 
-    def _build(self, system, **kwargs):
-        self._components = list()
-        for component in system.values():
-            if component.type == 'pv':
-                model = ModelChain(component, system._location, **dict(self.parameters), **kwargs)
-                
-                self._components.append(model)
+    def __init__(self, system, location, configs, **kwargs):
+        ModelChain.__init__(self, system, location, **dict(configs.items('Model')), **kwargs)
+        ModelCore.__init__(self, configs, system, **kwargs)
 
     def run(self, weather, **_):
-        components = dict()
-        
-        if self.threading:
-            import concurrent.futures as futures
-            # FIXME: Look into concurrency issues
-            split = 1 #os.cpu_count() or 1
-            
-            models = dict()
-            with futures.ThreadPoolExecutor() as executor:
-                weather_days = pd.Series(weather.index.date, index=weather.index)
-                for weather_range in np.array_split(list(set(weather.index.date)), split):
-                    weather_split = weather.loc[weather_days.isin(weather_range)]
-                    
-                    for model in self._components:
-                        models[executor.submit(model.run_model, weather_split)] = model
-                
-                for future in futures.as_completed(models):
-                    model = models[future]
-                    if model.system.id not in components:
-                        components[model.system.id] = list()
-                    
-                    components[model.system.id].append(model.ac);
-        else:
-            for model in self._components:
-                model.run_model(weather)
-                if model.system.id not in components:
-                    components[model.system.id] = list()
-                
-                components[model.system.id].append(model.ac);
-        
-        results = list()
-        for result in components.values():
-            results.append(pd.concat(result, axis=0))
-        
-        return pd.DataFrame(pd.concat(results, axis=1).sum(axis=1), columns=['p_mp'])
-
-
-class ModelChain(modelchain.ModelChain):
+        self.run_model(weather)
+        return self.ac
 
     def pvwatts_dc(self):
         self.dc = self.system.pvwatts_dc(self.effective_irradiance,
--- emonpv/system.py
+++ emonpv/system.py
@@ -9,29 +9,37 @@ import logging
 logger = logging.getLogger(__name__)
 
 import os
+import pandas as pd
+import datetime as dt
 
-from th_e_core import System as SystemCore
-from th_e_core import Model, Forecast
+from th_e_core import Forecast, ConfigUnavailableException
 from th_e_core.weather import Weather, TMYWeather, EPWWeather
 from th_e_core.pvsystem import PVSystem
+from th_e_core.system import System as SystemCore
 
 from pvlib.location import Location
 from configparser import ConfigParser
-from emonpv.database import ModuleDatabase #, InverterDatabase
+from emonpv.database import ModuleDatabase, InverterDatabase
+from emonpv.model import Model
 
 
 class System(SystemCore):
 
-    def _activate(self, components, *args, **kwargs):
-        super()._activate(components, *args, **kwargs)
+    def _activate(self, components, configs, **kwargs):
+        super()._activate(components, configs, **kwargs)
+        try:
+            self.weather = Forecast.read(self, **kwargs)
+            
+        except ConfigUnavailableException:
+            # Use weather instead of forecast, if forecast.cfg not present
+            self.weather = Weather.read(self, **kwargs)
         
-        self.weather = Weather.read(self, **kwargs)
         if isinstance(self.weather, TMYWeather):
             self.location = Location.from_tmy(self.weather.meta)
         elif isinstance(self.weather, EPWWeather):
             self.location = Location.from_epw(self.weather.meta)
-        
-        self._model = Model.read(self, **kwargs)
+        else:
+            self.location = self._location_read(configs, **kwargs)
 
     def _location_read(self, configs, **kwargs):
         return Location(configs.getfloat('Location', 'latitude'), 
@@ -59,36 +67,110 @@ class System(SystemCore):
         return super()._component(configs, type, **kwargs)
 
     def run(self, *args, **kwargs):
-        data = self._model.run(self.weather.get(*args, **kwargs), **kwargs)
         
-        if self._database is not None:
-            self._database.persist(data, **kwargs)
+        weather = self.weather.get(*args, **kwargs)
         
-        return data
+        result_columns = ['pv_power']
+        results = pd.DataFrame(columns=result_columns, index=weather.index).fillna(0)
+        results.index.name = 'Time'
+        
+        if len(self) > 0:
+            logger.info("Starting simulation for system: %s", self.name)
+            start = dt.datetime.now()
+            
+            for configs in self.values():
+                if configs.type == 'pv':
+                    model = Model.read(self, configs, **kwargs)
+                    result = model.run(weather, **kwargs)
+                    results['pv_power'] += result
+                    
+                    if self._database is not None:
+                        self._database.persist(result, **kwargs)
+            
+            logger.info('Simulation complete')
+            logger.debug('Simulation complete in %i seconds', (dt.datetime.now() - start).total_seconds())
+        
+        return pd.concat([results, weather], axis=1)
 
 
 class Configurations(PVSystem):
 
-    def _configure(self, configs, **_):
-        with open(os.path.join(configs['General']['config_dir'], configs['General']['id']+'.d', 'module.cfg')) as f:
-            module_file = '[Module]\n' + f.read()
+    def _configure(self, configs, **kwargs):
+        super()._configure(configs, **kwargs)
         
-        module_configs = ConfigParser()
-        module_configs.optionxform = str
-        module_configs.read_string(module_file)
+        self.module_parameters = self._configure_module(configs)
+        self.inverter_parameters = self._configure_inverter(configs)
+
+    def _configure_module(self, configs):
+        module = {}
         
-        self.module_parameters = {}
-        for key, value in module_configs.items('Module'):
-            try:
-                self.module_parameters[key] = float(value)
-                
-            except ValueError:
-                self.module_parameters[key] = value
+        if 'type' in configs['Module']:
+            type = configs['Module']['type'] #@ReservedAssignment
+            modules = ModuleDatabase(configs)
+            module = modules.get(type)
         
-        self.inverter_parameters = {}
-        if configs.has_section('Inverter') and 'pdc0' in self.module_parameters:
+        def module_update(items):
+            for key, value in items:
+                try:
+                    module[key] = float(value)
+                except ValueError:
+                    module[key] = value
+        
+        if configs.has_section('Parameters'):
+            module_update(configs.items('Parameters'))
+        
+        module_file = os.path.join(configs['General']['config_dir'], 
+                                   configs['General']['id']+'.d', 'module.cfg')
+        
+        if os.path.exists(module_file):
+            with open(module_file) as f:
+                module_str = '[Module]\n' + f.read()
+            
+            module_configs = ConfigParser()
+            module_configs.optionxform = str
+            module_configs.read_string(module_str)
+            module_update(module_configs.items('Module'))
+        
+        if 'pdc0' not in module:
+            module['pdc0'] = module['I_mp_ref'] \
+                           * module['V_mp_ref']
+        
+        return module
+
+    def _configure_inverter(self, configs):
+        inverter = {}
+        
+        if 'type' in configs['Inverter']:
+            type = configs['Inverter']['type'] #@ReservedAssignment
+            inverters = InverterDatabase(configs)
+            # TODO: Test and verify inverter CEC parameters
+            #inverter = inverters.get(type)
+        
+        def inverter_update(items):
+            for key, value in items:
+                try:
+                    inverter[key] = float(value)
+                except ValueError:
+                    inverter[key] = value
+        
+        inverter_file = os.path.join(configs['General']['config_dir'], 
+                                     configs['General']['id']+'.d', 'inverter.cfg')
+        
+        if os.path.exists(inverter_file):
+            with open(inverter_file) as f:
+                inverter_str = '[Inverter]\n' + f.read()
+            
+            inverter_configs = ConfigParser()
+            inverter_configs.optionxform = str
+            inverter_configs.read_string(inverter_str)
+            inverter_update(inverter_configs.items('Inverter'))
+        
+        if 'pdc0' not in inverter and 'pdc0' in self.module_parameters and \
+                configs.has_section('Inverter'):
             total = configs.getfloat('Inverter', 'strings') \
                   * configs.getfloat('Module', 'count')
             
-            self.inverter_parameters['pdc0'] = float(self.module_parameters['pdc0'])*total
+            inverter['pdc0'] = self.module_parameters['pdc0']*total
+        
+        return inverter
 
